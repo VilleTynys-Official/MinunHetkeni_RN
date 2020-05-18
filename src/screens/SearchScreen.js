4f65397c9bbe8fa7e49eb2753b7c7720f2ba5544
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-18 14:26:35 +0300

    Lisätty state searchbarin tekstille.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = ({ navigation }) => {
+    const [term, setTerm] = useState('');
+
     return(
         <>
-            <SearchBar/>
-            <Text style={StyleSheet.create({ fontSize: 50})} >SearchScreen</Text>
+            <SearchBar term= {term}
+                        onTermChange={newTerm =>setTerm(newTerm)}/>
+            <Text style={StyleSheet.create({ fontSize: 50})} >{term}</Text>
         </>
     )
     };
-    
+
 const styles = StyleSheet.create({});
 
 
