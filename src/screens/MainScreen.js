739bf54f698a09f3cfb74e04865a775e1009ac34
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-25 18:23:10 +0300

    kategorioille state ja funktio joka hakee kategoriat databasesta.

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,17 +1,28 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native';
 import CategoriesList from '../components/CategoriesList';
+import DataBaseInformation from '../data/DataBaseInformation.json';
+
+
 
 const MainScreen = () => {
-
-    //tiedot data-basesta:
     const [categories, setCategories] = useState([]);
-    
+
+    //haetaan kategoriat
+    const searchCategories = ()=>{
+        const response = DataBaseInformation;
+        setCategories(response)
+        console.log(response)
+    };
+
 
     return (
         <View>
             <Text style={StyleSheet.create({ fontSize: 50})} >MainScreen</Text>
             <Text>We have found: {categories.length} categories</Text>
+            <Button title='Hae kategoriat'
+                    onPress= {searchCategories}
+                    />
             <CategoriesList title='Ensimmäiset askeleet' />
             <CategoriesList title='Metta meditointi' />  
             <CategoriesList title='Hengitysharjoitukset' />  
