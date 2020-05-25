47551a4dca07366f7959b5facfa6beecc621acc2
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-25 22:54:55 +0300

    Lisätty Flatlist.

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -7,11 +7,18 @@ import {View, Text, StyleSheet, Image, FlatList} from 'react-native';
 
 //"pidetty tyhmänä". Näyttää kaikki meditaatiot jotka annetaan sisälle.
 const CategoriesList = ({title, categories}) => {
-
     return (
         <View>
             <Text style= {styles.title}> {title}</Text>
             <Text>kursseja:{categories.length}</Text>
+            <FlatList 
+                horizontal
+                data={categories}
+                keyExtractor={ (category) => category.nimi} //TODO: uniikit Id numerot..
+                renderItem = {( {item }) => {
+                    return <Text>{item.nimi}    </Text>
+                }}
+            />
         </View>
     )
 };
