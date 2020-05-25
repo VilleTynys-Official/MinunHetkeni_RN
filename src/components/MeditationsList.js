ce51615ad56367b92325f26a44266005393a2447
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-25 14:26:47 +0300

    Luotu MeditationsList komponentti.

---
+++ src/components/MeditationsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const MeditationsList = () => {
+    return (
+        <>
+            <Text> Meditaatio</Text>
+        </>
+    )
+};
+
+
+const styles = StyleSheet.create({});
+
+
+export default MeditationsList;
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import MeditationsList from '../components/MeditationsList';
 
 const MainScreen = () => {
     return (
         <View>
             <Text style={StyleSheet.create({ fontSize: 50})} >MainScreen</Text>
+            <MeditationsList></MeditationsList>
+            <MeditationsList></MeditationsList>
+            
         </View>
     );
 };
