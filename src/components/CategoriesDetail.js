fd549d6b16acf18b7880960e33bfde6c8e748b09
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-26 12:26:26 +0300

    lisätty kuva joka haetaan url:sta.

--- src/components/CategoriesDetail.js
+++ src/components/CategoriesDetail.js
@@ -3,14 +3,16 @@ import {View, Image, Text, StyleSheet } from 'react-native';
 
 /**
  * Kategorioiden muotoilu tapahtuu täällä.
+ * 
+ * kuvan sourcena käytetty random kuvaa internetistä. (myöhemmin omat kuvat backendistä)
  */
 
 const CategoriesDetail = ({category}) =>{
     return (
         <View>
-            <Image source={ require('../imgs/kategoria1.png')}
+            <Image source={{uri: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1557258847-chinese-evergreen-houseplant-1557258690.jpg?crop=0.883xw:0.887xh;0.0849xw,0.0821xh&resize=480:*'}}
                     style={styles.image} />
-            <Text>{category.nimi}     </Text>
+            <Text>{category.nimi}   </Text>
         </View>
     )
 };
