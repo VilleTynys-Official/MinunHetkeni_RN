cf8ca0797b9d15b9e700e784a74e983c2e4fa361
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-27 12:08:49 +0300

    Välitetty navigoinnin avulla tieto valitusta kategoriasta.

--- src/screens/LessonsScreen.js
+++ src/screens/LessonsScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const LessonsScreen = () => {
+/**TODO
+ * 0 Luodaan lessonien tiedot.
+ * 1 haetaan valitun kategorian lessonien tiedot.
+ * 2 renderöidään ne listaksi (LessonsList komponentilla.)
+ * 3 lisätään navigointi mediaScreeniin.
+ */
+
+
+const LessonsScreen = ({navigation}) => {
+    //console.log(navigation)
+    //console.log(navigation.getParam('kategoria_id'))
+    const id = navigation.getParam('kategoria_id')
+
     return (
-        <Text style={StyleSheet.create({ fontSize: 50})} >LessonsScreen</Text>
+        <Text style={StyleSheet.create({ fontSize: 50})} >{id}</Text>
     )
 };
 
