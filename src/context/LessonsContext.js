7950f16271639b177ae434408e03355bdad9118c
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-04 09:23:14 +0300

    Luotu Contextiin state valitulle lessonille ja action jolla sitä voidaan muokata. (koska halutaan et valitun lessonin state on käytettävissä kaikkialla ohjelmassa).

--- src/context/LessonsContext.js
+++ src/context/LessonsContext.js
@@ -12,23 +12,22 @@ import createDataContext from './createDataContext';
 //Reducer
 const lessonsReducer = (state, action) => {
     switch (action.type) {
-        case 'change_chosen_lesson':
-            return {chosenLesson: action.payload };
+        case 'set_chosen_lesson':
+            return {...state, chosenLesson: action.payload };
         default:
             return state;
     };
 };
 
-
 //actionit
-const changeChosenLesson = dispatch =>(lesson) => {
-    
-    dispatch ({ type: 'change_chosen_lesson', payload: lesson })
+const setChosenLesson = dispatch =>(lesson) => {
+    console.log(lesson)
+    dispatch ({ type: 'set_chosen_lesson', payload: lesson });
 };
 
 //hyödynnetään createDataContext komponenttia ja luodaan Contexti.
 export const { Provider, Context} = createDataContext(
     lessonsReducer,
-    { changeChosenLesson },
-    { chosenLesson: ''}
+    { setChosenLesson },
+    { chosenLesson:'default lesson'}
 );
--- src/screens/LessonsScreenTab.js
+++ src/screens/LessonsScreenTab.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {View, Button, Text, StyleSheet, ScrollView} from 'react-native';
 import useCategories from '../hooks/useCategories';
 import CategoriesList from '../components/CategoriesList';
 import { FlatList } from 'react-native-gesture-handler';
 import LessonsList from '../components/LessonsList';
 import useNavigation from '@react-navigation/native';
 import CategoriesCarousel from '../components/CategoriesCarousel';
+import { Context as LessonsContext} from '../context/LessonsContext';
+
 /**TODO
  * 1 haetaan KAIKKIEN kategorioiden tiedot useCategories avulla. OK!
  * 2 Näytetään KAIKKI carousel viewssä (esim.  pelkät kategorian nimet tai kuvat)
@@ -17,31 +19,23 @@ import CategoriesCarousel from '../components/CategoriesCarousel';
 const LessonsScreenTab = (navigation) => {
     // const [category, setCategory] = useState(null);
     const categories = useCategories();
-    const [lessons, setLessons] = useState([]);
-
+    const { state: {chosenLesson}, setChosenLesson} = useContext(LessonsContext);
 
     //TODO: Opettele tämä hyvin!!!!!
     // ES6 MÄPPÄÄMINEN: uusi array, jossa on kaikki funktion perusteella mäpätyt jäsenet.
     // eli siis näin voidaan filtteröidä JSON datasta "key" arvoja hyödyntämällä.
     // console.log(categories.map(JSONkey => JSONkey.nimi ))
-    const images = categories.map(JSONkey => JSONkey.image_url)
-    console.log(images)    
+    const images = categories.filter(JSONkey => JSONkey.image_url)
 
-
-
-
+    console.log(chosenLesson)
 
     //TODO: provider joka tuo lessonin staten?
     return (
             <View>
-                {/* <CategoriesCarousel
-                                isHorizontal={true}
-                                setLessons ={()=>setLessons()}
-                                categories={categories}/> */}
-                <LessonsList lessons ={lessons}
-                />
+                <Text>{chosenLesson}</Text>
+                <Button title='vaihda lesson'
+                        onPress={()=>setChosenLesson('lkj')}></Button>
             </View>
-
     );
 };
 
