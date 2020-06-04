935d78bbe9787f67949ab8c942f525e1c7b008c7
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-04 08:39:45 +0300

    createDataContext tuotu toisesta projektista. Tällä luodaan Context Providerit.

---
+++ src/context/createDataContext.js
@@ -0,0 +1,29 @@
+import React, { useReducer } from "react";
+//tällä komponentilla siis luodaan Contexteja (joissa on statet ja funktiot joilla stateja voi muuttaa).
+
+
+
+export default (reducer, actions, defaultValue) =>{
+    const Context = React.createContext();
+
+    //dispatchia kutsutaan jollain actionilla ja päivittää tilan..
+    const Provider = ({ children}) => {
+        const [state, dispatch] = useReducer(reducer, defaultValue); //provider on niinku wrapperi eri stateille ja niitä muuttaville funktioille.
+        
+        
+        const boundActions = {};
+        for (let key in actions) {
+            boundActions[key] = actions[key](dispatch);
+        }
+
+        return (
+            <Context.Provider value={{ state, ...boundActions}}>
+                {children}
+            </Context.Provider>
+        );
+    };
+
+
+    return { Context, Provider };
+};
+
