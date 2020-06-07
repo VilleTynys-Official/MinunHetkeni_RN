4844b4cceb6a2a57337c281bfe4cf6970bbf10a9
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-07 09:49:26 +0300

    Korjattu nimi paremmaksi.

--- src/components/CategoriesCarousel.js
+++ src/components/CategoriesCarousel.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import GallerySwiper from 'react-native-gallery-swiper';
 import {View, Button, Text, StyleSheet, ScrollView} from 'react-native';
 
-const categoriesCarousel = () =>{
+
+/**
+ * Näyttää kuvat kaikista kategorioista (halutussa muodossa), jotka annetaan sisälle.
+ * Sisältää mahdollisuuden hyödyntää myös categorioista muitakin tietoja
+ * TODO
+ * 1. Otetaan kaikki kategoriat kuvineen käyttöön.
+ * 2. Detailssit kuntoon
+ * 3. lessonContextin muuttaminen kun käyttäjä swipee kategorioita. (ehkä johonkin muualle?)
+ * 
+ * .
+*/
+
+const CategoriesCarousel = ({categories}) =>{
+    // useEffect(()=>{
+    //     const image_url = categories.map(JSONKEY => JSONKEY.image_url)
+    // })
+    
+    const images = categories.map(JSONKEY => JSONKEY.image_url)
+    console.log(images)
+
 
     return (
             <>
@@ -10,7 +29,9 @@ const categoriesCarousel = () =>{
                 style={styles.carouselStyle}
                 enableScale={false}
                 pageMargin={40}                
-                images={[
+                images={
+
+                    [
                     { source: require("../imgs/kategoria1.png"),
                     // Version *1.5.0 update (or greater versions):
                     // An alternative to the dimensions field.
@@ -24,8 +45,9 @@ const categoriesCarousel = () =>{
                     // This will also be acceptable.
                     width: 1080,
                     height: 1920 },
+                    ]
                     
-                    ]}>
+                    }>
 
                 </GallerySwiper>
             </>
@@ -41,5 +63,5 @@ const categoriesCarousel = () =>{
 
 
 
-export default categoriesCarousel;
+export default CategoriesCarousel;
 
