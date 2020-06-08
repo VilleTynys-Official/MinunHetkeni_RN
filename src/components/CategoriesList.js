e61a55aa41374588dd81ec9fdf1b8a3b649845f5
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-08 13:52:35 +0300

    chosenCategoryn päivittäminen lisätty.

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {View, Text, StyleSheet, Image, FlatList, TouchableOpacity} from 'react-native';
 import CategoriesDetail from './CategoriesDetail';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native'
+import { Context as CategoriesContext} from '../context/CategoriesContext';
 //komponentti "pidetty tyhmänä" >>> Näyttää kaikki kategoriat jotka annetaan sisälle.
 //TODO: Päivitä Context kun kategoriaa painetaa.
 
 const CategoriesList = ({title, categories, navigation, isHorizontal}) => {
     navigation = useNavigation();
-    
+    const {state: {chosenCategory}, setChosenCategory} = useContext(CategoriesContext);
+    // console.log(chosenCategory)
 
     return (
         <View style={styles.container}>
@@ -21,10 +23,8 @@ const CategoriesList = ({title, categories, navigation, isHorizontal}) => {
                 renderItem = {( {item }) => {
                     return (
                     <TouchableOpacity onPress={()=> {
-                        console.log('esim täs kohtaa voi päivittää lessons staten provideriin (luento sivu saa tiedon mistä aloittaa carousel..)');
-                        
-                        
-                        
+                        // console.log(item.kategoria_id)
+                        setChosenCategory(item.kategoria_id)
                         // nyt tieto viedään propseina, mut provider vois toimia paremmin:
                             navigation.navigate('Lessons', {kategoria_id: item.kategoria_id, lessons: item.lessons})}}>
                         <CategoriesDetail category={item} />
