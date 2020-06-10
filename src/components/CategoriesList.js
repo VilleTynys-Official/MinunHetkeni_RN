b194525ae65afc04625f2722254beb7a8d5ab2b9
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-10 14:26:39 +0300

    Siistitty ja asetettu navigointi LessonsTabScreeniin kun kategoria valitaan.

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,35 +1,36 @@
 import React, { useContext } from 'react';
-import {View, Text, StyleSheet, Image, FlatList, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
 import CategoriesDetail from './CategoriesDetail';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native'
-import { Context as CategoriesContext} from '../context/CategoriesContext';
+import { Context as CategoriesContext } from '../context/CategoriesContext';
 
 
 //komponentti "pidetty tyhmänä" >>> Näyttää kaikki kategoriat jotka annetaan sisälle.
 
-const CategoriesList = ({title, categories, navigation, isHorizontal}) => {
+const CategoriesList = ({ title, categories, navigation, isHorizontal }) => {
     navigation = useNavigation();
-    const {state: {chosenCategory}, setChosenCategory} = useContext(CategoriesContext);
+    const { state: { chosenCategory }, setChosenCategory } = useContext(CategoriesContext);
     // console.log(chosenCategory)
 
     return (
         <View style={styles.container}>
-            <Text style= {styles.title}> {title}</Text>
-            <FlatList 
+            <Text style={styles.title}> {title}</Text>
+            <FlatList
                 horizontal={isHorizontal}   //voidaan säätää propsilla suunta.
                 nestedScrollEnabled={false}
                 data={categories}
-                keyExtractor={ (category) => category.kategoria_id}
-                renderItem = {( {item }) => {
+                keyExtractor={(category) => category.kategoria_id}
+                renderItem={({ item }) => {
                     return (
-                    <TouchableOpacity onPress={()=> {
-                        // console.log(item.kategoria_id)
-                        setChosenCategory(item.kategoria_id)
-                        // TODO: navigointi onnistuu, mut mites propsit ja onko tää kuinka raskas menetelmä mennä suoraan?
-                            navigation.navigate('Lessons', {kategoria_id: item.kategoria_id, lessons: item.lessons})}}>
-                        <CategoriesDetail category={item} />
-                    </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                // console.log(item.kategoria_id)
+                                setChosenCategory(item.kategoria_id)
+                                navigation.navigate('LessonsTab', { kategoria_id: item.kategoria_id, lessons: item.lessons })
+                            }}>
+                            <CategoriesDetail category={item} />
+                        </TouchableOpacity>
                     )
                 }}
             />
