f168b63b35d8289de09ae733322a3214af2a0ef1
V.Tynys <ville.tynys@gmail.com>
2020-07-08 20:42:36 +0300

    TODO poistettu ja kommentit siistitty koska kaikki on OK.

--- src/screens/LessonsScreenTab.js
+++ src/screens/LessonsScreenTab.js
@@ -6,27 +6,13 @@ import { Context as CategoriesContext } from '../context/CategoriesContext';
 import useCategories from '../hooks/useCategories';
 import { ScrollView } from 'react-native-gesture-handler';
 
-
-/**TODO
- * 1 haetaan KAIKKIEN kategorioiden tiedot useCategories avulla. OK
- * 2 Näytetään KAIKKI carousel viewssä (esim.  pelkät kategorian nimet tai kuvat) OK
- *  
- * 
- * 3 Päivitetään lessonit alle sen mukaisesti mikä kategoria on näkyvillä karusellissa. >>>NEXT
- *      eli ns. yksi totuus mikä category on valittu.
- * 
- * 4 Yhdistetään main pagen navigointiin (et sieltä valinta tuo tänne.) OK
- * 
- */
-
 const LessonsScreenTab = (navigation) => {
     const { state: { chosenCategory } } = useContext(CategoriesContext);
-    const categories = useCategories()
+    const categories = useCategories() //haetaan KAIKKIEN kategorioiden tiedot useCategories avulla.
 
-    //TODO: Tähän funktio joka palauttaa lessons[] jossa on chosenCategoryyn kuuluvat lessonit.
+    // ladataan lessons[] chosenCategoryyn kuuluvilla lessoneilla.
     // console.log('lessonit:', categories.filter(category => category.kategoria_id== '0001'))
     const [lessons] = categories.filter(category => category.kategoria_id == chosenCategory).map(x => x.lessons)
-    // console.log('***', lessons)
 
 
     return (
